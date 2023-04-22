7720e0c969826f219db050004667c6cea8c29df9
belac <belacaste3@gmail.com>
2023-04-22 15:37:46 -0500

    commit

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from interfaz import *
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
-    inicio = Seleccion()
+    inicio = Ventana_principal()
     inicio.show()
     sys.exit(app.exec())
